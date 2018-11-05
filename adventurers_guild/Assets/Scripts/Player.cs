935491c2b9878e4aa03ae85fb4a3350acaa03dba
toxicRooster <seeger01@gmail.com>
2018-11-05 18:04:55 +1100

    Adding in health and mana bars and basic controller for them

--- adventurers_guild/Assets/Scripts/Player.cs
+++ adventurers_guild/Assets/Scripts/Player.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Player : Character
 {
+    [SerializeField]
+    private Stat health;
+    private float initialHealth = 100;
+
+    [SerializeField]
+    private Stat mana;
+    private float initialMana = 100;
 
     // Use this for initialization
     protected override void Start()
     {
+        health.Initialise(initialHealth, initialHealth);
+        mana.Initialise(initialMana, initialMana);
         base.Start();
     }
 
@@ -23,6 +32,20 @@ public class Player : Character
 
     private void GetInput()
     {
+
+
+        if (Input.GetKey(KeyCode.I))
+        {
+            health.MyCurrentValue -= 10;
+            mana.MyCurrentValue -= 10;
+        }
+
+        if (Input.GetKey(KeyCode.O))
+        {
+            health.MyCurrentValue += 10;
+            mana.MyCurrentValue += 10;
+        }
+
 
         direction = Vector2.zero;
 
---
+++ adventurers_guild/Assets/Scripts/Stat.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stat : MonoBehaviour {
+
+    private Image content;
+
+    private float currentFill;
+
+    [SerializeField]
+    private float lerpSpeed;
+
+    public float MyMaxValue { get; set; }
+
+    public float MyCurrentValue
+    {
+        get
+        {
+            return currentValue;
+        }
+
+        set
+        {
+            if(value > MyMaxValue)
+            {
+                currentValue = MyMaxValue;
+            }
+            else if(value < 0)
+            {
+                currentValue = 0;
+            }
+            else
+            {
+                currentValue = value;
+            }
+
+            currentFill = currentValue / MyMaxValue;
+        }
+    }
+
+    private float currentValue;
+
+
+    // Use this for initialization
+    void Start () {
+        content = GetComponent<Image>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(currentFill != content.fillAmount)
+        {
+            content.fillAmount = Mathf.Lerp(content.fillAmount, currentFill, Time.deltaTime * lerpSpeed);
+        }
+	}
+
+    public void Initialise(float currentValue, float maxValue)
+    {
+        MyMaxValue = maxValue;
+        MyCurrentValue = currentValue;
+    }
+
+}
