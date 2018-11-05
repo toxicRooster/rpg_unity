fef964287dbff3b2329a28c3053129758330d99f
toxicRooster <seeger01@gmail.com>
2018-11-05 14:50:08 +1100

    Adding in walk animations for player character

--- adventurers_guild/Assets/Scripts/Character.cs
+++ adventurers_guild/Assets/Scripts/Character.cs
@@ -29,11 +29,24 @@ public abstract class Character : MonoBehaviour
     {
         transform.Translate(direction * speed * Time.deltaTime);
 
-        AnimateMovement(direction);
+        if(direction.x != 0 || direction.y != 0)
+        {
+            AnimateMovement(direction);
+        }
+        else
+        {
+            // change the animator layer back to idle
+            animator.SetLayerWeight(animator.GetLayerIndex("WalkLayer"), 0);
+        }
+        
     }
 
     public void AnimateMovement(Vector2 direction)
     {
+        // Set the weight of the animator layer to chnage to walk animation when moving
+        animator.SetLayerWeight(animator.GetLayerIndex("WalkLayer"), 1);
+
+        //Set the animator parameters to make the character face the correct way
         animator.SetFloat("x", direction.x);
         animator.SetFloat("y", direction.y);
     }
